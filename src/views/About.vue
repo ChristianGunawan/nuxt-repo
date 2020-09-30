8817c29f5cf7599a28f33107bb6c3d1a03350677
Christian Gunawan <57822261+ChristianGunawan@users.noreply.github.com>
2020-09-30 20:12:38 +0700

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="about">
+  <p style="width: 300px; text-align: justify;">Perkenalkan, namaku Christian Gunawan dengan NIM 13519199 dari Teknik Informatika '19. Pada kali ini mengambil bagian di divisi maintanance pada Parade Wisuda Oktober 2020</p>
+</div>
+</template>
+
+<style scoped>
+  template{
+	background-color:black
+  }
+  div{
+  background-color: aliceblue}
+  p:hover{
+    background-color:salmon;
+  }
+</style>
